04a3bc97ae3a0f767beacc0105c21ad498ca0235
robertelias <robert.elias@live.com>
2020-05-13 14:12:24 -0700

    MVP MET

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
-select ProductName, CategoryName from Product
-join Category 
-on Product.CategoryId = Category.Id;
+SELECT ProductName, CategoryName FROM Product
+JOIN Category 
+ON Product.CategoryId = Category.Id;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 SELECT [Order].Id, Shipper.CompanyName 
 FROM [Order]
