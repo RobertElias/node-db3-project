4ef3165834188a4a1be4762e594577c861e83129
robertelias <robert.elias@live.com>
2020-05-13 14:05:05 -0700

    -- Display the order Id and shipper CompanyName

--- queries.sql
+++ queries.sql
@@ -5,10 +5,11 @@ select ProductName, CategoryName from Product
 join Category 
 on Product.CategoryId = Category.Id;
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-select [order]Id, Shipper.CompanyName from [Order]
-join Shipper
-on [Order].ShipVia = Shipper.Id
-where ShippedDate <= '2012-08-09'
+SELECT [Order].Id, Shipper.CompanyName 
+FROM [Order]
+JOIN Shipper
+ON [Order].ShipVia = Shipper.Id
+WHERE OrderDate <= '2012-08-09'
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
